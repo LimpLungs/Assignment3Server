ead30d508b3b9a5af7b5ac606dd2df9450439e2b
sieverslr06 <sieverslr06@HY210-04-LP>
2022-02-17 17:36:49 -0600

    Add project files.

---
+++ Assignment3Server/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Assignment3Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartServer();
+        }
+
+        String GLOBAL_LOCAL_IP = "127.0.0.1";
+
+        private static void StartServer()
+        {
+            Console.WriteLine("Starting server...");
+
+
+        }
+    }
+}
